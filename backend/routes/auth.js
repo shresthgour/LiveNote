194a4bf78f2a6e5e433672a321857206b6271844
Shresth Gour <akshat4575@gmail.com>
2023-04-13 15:04:49 +0530

    added a authorization route

---
+++ backend/routes/auth.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+
+
+
+// * Create A User using: POST "/api/auth/". Doesn't require Auth
+router.post('/',[
+  body('name', 'Enter a valid name').isLength({min: 3}),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password must be atleast 5 characters').isLength({min: 5})
+], (req, res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err => {console.log(err)
+  res.json({error: 'Please enter a unique value for Email', message: err.message})})
+});
+module.exports = router;
